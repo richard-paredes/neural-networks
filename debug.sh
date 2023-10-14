08d66cd431144bf57e1fecaa1e9d3f8fdd73e054
Richard Paredes <richard.o.paredes@gmail.com>
2023-10-14 15:16:36 -0500

    added minimal layout

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
 # TODO: Learn how to run these processes in the background and terminate once this script is exited
-npx tailwindcss -i ./Styles/app.css -o ./wwwroot/css/app.css --watch
-dotnet watch --project src/StoryMaster
+npx tailwindcss -i ./src/StoryMaster/Styles/app.css -o ./src/StoryMaster/wwwroot/css/app.css --watch
+dotnet watch --project ./src/StoryMaster
 
